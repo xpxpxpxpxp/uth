00dbf28c5dc97d6f2e080299d717ee589fac1a81
xpxpxpxpxp <xpxpxpxpxp0404@hotmail.com>
2013-12-28 19:07:50 +0800

    fix some bugs.

--- th.c
+++ th.c
@@ -5,10 +5,44 @@
 #include <assert.h>
 #include <sched.h>
 #include <unistd.h>
+#include <string.h>
 #include "th.h"
 
 #define NR_CPU      1
 #define STACK_SIZE  8192
+#define USE_ATOMIC_BY_SPIN
+
+#ifdef USE_ATOMIC_BY_SPIN
+
+static pthread_spinlock_t  g_atomic_lock;
+
+static inline void atomic_bit_set(int *n, int mask)
+{
+    pthread_spin_lock(&g_atomic_lock);
+    (*n) |= mask;
+    pthread_spin_unlock(&g_atomic_lock);
+}
+
+static inline void atomic_bit_clear(int *n, int mask)
+{
+    pthread_spin_lock(&g_atomic_lock);
+    (*n) &= ~mask;
+    pthread_spin_unlock(&g_atomic_lock);
+}
+
+static inline int atomic_bit_test_and_set(int *n, int mask)
+{
+    int last;
+
+    pthread_spin_lock(&g_atomic_lock);
+    last = *n;
+    (*n) |= mask;
+    pthread_spin_unlock(&g_atomic_lock);
+    
+    return last;
+}
+
+#endif
 
 typedef void (*ctx_func_t)(void);
 
@@ -32,25 +66,25 @@ static inline int ts_after(struct timespec *a, struct timespec *b)
 
 static inline void get_wait_ts(struct cpu *cpu, struct timespec *ts)
 {
-    struct th   *th;
+    struct evt   *evt;
 
     assert(!l_empty(&cpu->sleep));
 
-    th = l_entry(l_first(&cpu->sleep), struct th, snode);
-    *ts = th->sleep;
+    evt = l_entry(l_first(&cpu->sleep), struct evt, node);
+    *ts = evt->sleep;
 }
 
 static inline void append_to_ready_unlocked(struct th *th, struct cpu *cpu)
 {
     l_add_tail(&th->node, &cpu->ready);
-    th->state = TH_READY;
+    atomic_bit_set(&th->state, TH_READY);
 }
 
 static inline void append_to_ready(struct th *th, struct cpu *cpu)
 {
     pthread_spin_lock(&cpu->spin);
     l_add_tail(&th->node, &cpu->ready);
-    th->state = TH_READY;
+    atomic_bit_set(&th->state, TH_READY);
     pthread_spin_unlock(&cpu->spin);
 }
 
@@ -62,38 +96,37 @@ static inline struct th *pop_from_ready(struct cpu *cpu)
     pthread_spin_lock(&cpu->spin);
     l_del(&th->node);
     pthread_spin_unlock(&cpu->spin);
-    th->state = TH_RUN;
+    th->state |= TH_RUN;
+    th->state &= ~TH_READY;
     return th;
 }
 
-static void wakeup_sleep_if_need(struct cpu *cpu)
+static void do_event(struct cpu *cpu)
 {
     struct timespec ts;
-    struct th       *th;
+    struct evt      *evt;
 
+    assert(sched_getcpu() == cpu->id);
+    
     if (l_empty(&cpu->sleep))
         return;
 
-    clock_gettime(CLOCK_REALTIME, &ts);
-
-    pthread_spin_lock(&cpu->spin);
-    th = l_entry(l_first(&cpu->sleep), struct th, snode);
-    
-    if (ts_after(&ts, &th->sleep)) {
-        l_del(&th->snode);
-        if (th->state & (TH_WAIT_LK|TH_WAIT_CD)) {
-            l_del(&th->node);
+    while (!l_empty(&cpu->sleep)) {
+        evt = l_entry(l_first(&cpu->sleep), struct evt, node);
+        clock_gettime(CLOCK_REALTIME, &ts);
+        if (!ts_after(&ts, &evt->sleep)) {
+            break;
         }
-        append_to_ready_unlocked(th, cpu);
+        l_del(&evt->node);
+        evt->func(evt->arg1, evt->arg2, evt->arg3);
     }
-    pthread_spin_lock(&cpu->spin);
 }
 
 static void sched(struct cpu *cpu)
 {
     struct th   *old = cpu->run;
 
-    wakeup_sleep_if_need(cpu);
+    do_event(cpu);
     while (l_empty(&cpu->ready)) {
         struct timespec ts0, ts1;
 
@@ -110,7 +143,7 @@ static void sched(struct cpu *cpu)
         pthread_mutex_lock(&cpu->mutex); 
         pthread_cond_timedwait(&cpu->cond, &cpu->mutex, &ts0);
         pthread_mutex_unlock(&cpu->mutex); 
-        wakeup_sleep_if_need(cpu);
+        do_event(cpu);
     }
 
     cpu->run = pop_from_ready(cpu);
@@ -118,6 +151,11 @@ static void sched(struct cpu *cpu)
     if (old==cpu->run)
         return;
 
+    if (cpu->run->state & TH_SLEEP) {
+        cpu->run->state &= ~TH_SLEEP;
+        l_del(&cpu->run->evt.node);
+    }
+    
     if (old) {
         swap_ctx(&old->uc, &cpu->run->uc);
     } else {
@@ -160,6 +198,9 @@ static void init_cpu(void)
     int         i;
     struct cpu  *cpu;
 
+#ifdef USE_ATOMIC_BY_SPIN
+    pthread_spin_init(&g_atomic_lock, PTHREAD_PROCESS_PRIVATE);
+#endif
     for (i=0; i<NR_CPU; i++) {
         cpu = &g_cpu[i];
         cpu->id = i;
@@ -183,7 +224,7 @@ struct th *th_create(int cpu_id, void (*func)(struct th *), void *arg)
     buf = malloc(sizeof(struct th) + STACK_SIZE);
     assert(buf);
 
-
+    bzero((char *)buf, sizeof(sizeof(struct th) + STACK_SIZE));
     th = (struct th *)buf;
     get_ctx(&th->uc);
     th->uc.uc_stack.ss_sp = buf + sizeof(struct th);
@@ -194,7 +235,7 @@ struct th *th_create(int cpu_id, void (*func)(struct th *), void *arg)
     th_cd_init(&th->done_cd);
     th->arg = arg;
     l_init(&th->node);
-    l_init(&th->snode);
+    l_init(&th->evt.node);
     make_ctx(&th->uc, (ctx_func_t)func, 1, th);
     append_to_ready(th, cpu);
     pthread_cond_signal(&cpu->cond);
@@ -211,37 +252,50 @@ static struct th *th_self(void)
 
 static void insert_to_sleep_q(struct th *th, unsigned long usec)
 {
-
-    struct th       *c;
-    struct timespec *ts = &th->sleep;
+    struct evt      *evt;
+    struct timespec *ts = &th->evt.sleep;
     struct cpu      *cpu = &g_cpu[th->cpu];
     struct lh       *lh; 
 
+    assert(th->cpu == sched_getcpu());
+    
     clock_gettime(CLOCK_REALTIME, ts);
     ts->tv_nsec += usec * 1000;
     ts_adjust(ts);
 
-    pthread_spin_lock(&cpu->spin);
     for (lh=cpu->sleep.next; lh!=&cpu->sleep; lh=lh->next) {
-        c = l_entry(lh, struct th, snode);
-        if (ts_after(ts, &c->sleep))
+        evt = l_entry(lh, struct evt, node);
+        if (!ts_after(ts, &evt->sleep))
             break;
     }
     /* insert before lh */
-    l_add_raw(&th->snode, lh->prev, lh);
-    pthread_spin_unlock(&cpu->spin);
+    l_add_raw(&th->evt.node, lh->prev, lh);
+}
+
+static void wakeup_by_sleep(void *arg1, long arg2, long arg3)
+{
+    struct th   *th = (struct th *)arg1;
+
+    assert(th->cpu == sched_getcpu());
+
+    th->state &= ~TH_SLEEP;
+    append_to_ready(th, &g_cpu[th->cpu]);
+    /* it is not necessary signal myself :) */
+    //pthread_cond_signal(&g_cpu[th->cpu].cond);
 }
 
 int th_usleep(unsigned long usec)
 {
     struct th       *th = th_self();
+    struct evt      *evt;
     struct cpu      *cpu = &g_cpu[th->cpu];
 
-    pthread_spin_lock(&cpu->spin);
-    l_del(&th->node);
+    evt = &th->evt;
+    evt->func = wakeup_by_sleep;
+    evt->arg1 = th;
     insert_to_sleep_q(th, usec);
-    th->state = TH_SLEEP; 
-    pthread_spin_unlock(&cpu->spin);
+    th->state &= ~TH_RUN;
+    th->state |= TH_SLEEP; 
     sched(cpu);
 
     return 0;
@@ -259,6 +313,7 @@ int th_yield(void)
         return 0;
     }
 
+    th->state &= ~TH_RUN;
     append_to_ready_unlocked(th, cpu);
     pthread_spin_unlock(&cpu->spin);
 
@@ -326,7 +381,8 @@ int th_lk_lock(struct lk *lk)
         struct th   *th = th_self();
 
         l_add_tail(&th->node, &lk->wait);
-        th->state = TH_WAIT_LK; 
+        th->state |= TH_WAIT_LK;
+        th->state &= TH_RUN;
         pthread_spin_unlock(&lk->spin);
         sched(&g_cpu[th->cpu]);
         pthread_spin_lock(&lk->spin);
@@ -351,6 +407,7 @@ int th_lk_unlock(struct lk *lk)
     if (th) {
         struct cpu *cpu = &g_cpu[th->cpu];
 
+        th->state &= ~TH_WAIT_LK;
         append_to_ready(th, cpu);
         pthread_cond_signal(&cpu->cond);
     }
@@ -372,7 +429,8 @@ int th_cd_wait(struct cd *cd)
         struct th   *th = th_self();
         
         l_add_tail(&th->node, &cd->wait);
-        th->state = TH_WAIT_CD; 
+        th->state |= TH_WAIT_CD; 
+        th->state &= ~TH_RUN;
         pthread_spin_unlock(&cd->spin);
         sched(&g_cpu[th->cpu]);
         pthread_spin_lock(&cd->spin);
@@ -382,26 +440,71 @@ int th_cd_wait(struct cd *cd)
     return 0;
 }
 
+static void wakeup_by_cd_sleep(void *arg1, long arg2, long arg3)
+{
+    struct th    *th = (struct th *)arg1;
+    struct cd    *cd = (struct cd *)arg2;
+
+    assert(th->cpu == sched_getcpu());
+
+    pthread_spin_lock(&cd->spin);
+    if (!(th->state & TH_WAIT_CD)) {
+        pthread_spin_unlock(&cd->spin);
+        /* 
+         * NOTE!!! 
+         * We already have been waken up by th_cd_signal or th_cd_bcast.
+         * This means another thread hold me, and it is maybe modifying my state (th->state),
+         * so, we must use atomic_bit_clear to avoid race.
+         */
+        atomic_bit_clear(&th->state, TH_SLEEP);
+        return;
+    }
+    
+    th->state &= ~TH_WAIT_CD;
+    l_del(&th->node);
+    pthread_spin_unlock(&cd->spin);
+
+    th->wakeup_expired = 1;
+    th->state &= ~TH_SLEEP;
+    append_to_ready(th, &g_cpu[th->cpu]);
+    /* it is not necessary signal myself :) */
+    //pthread_cond_signal(&g_cpu[th->cpu].cond);
+}
+
+
 
 int th_cd_timedwait(struct cd *cd, unsigned long usec)
 {
     pthread_spin_lock(&cd->spin);
     while (cd->val<=0) {
         struct th   *th = th_self();
-        struct cpu  *cpu = &g_cpu[th->cpu];
-        
+
+        th->wakeup_expired = 0;
         l_add_tail(&th->node, &cd->wait);
-        pthread_spin_lock(&cpu->spin);
-        th->state = TH_WAIT_CD|TH_SLEEP; 
-        insert_to_sleep_q(th, usec);
-        pthread_spin_unlock(&cpu->spin);
+        th->state |= TH_WAIT_CD; 
+        th->state &= ~TH_RUN;
         pthread_spin_unlock(&cd->spin);
+
+        /*
+         * NOTE:
+         * Once we have linked in cd->wait, we can be accessed by the other thread immediately. 
+         * for example, by the thread calling th_cd_signal on another core ..
+         * so, we must use atomic_bit_set to avoid race.
+         */
+        atomic_bit_set(&th->state, TH_SLEEP);
+        th->evt.func = wakeup_by_cd_sleep;
+        th->evt.arg1 = th;
+        th->evt.arg2 = (long)cd;
+        insert_to_sleep_q(th, usec);
         sched(&g_cpu[th->cpu]);
+        if (th->wakeup_expired) {
+            return 0;
+        }
         pthread_spin_lock(&cd->spin);
     }
     cd->val --;
     pthread_spin_unlock(&cd->spin);
-    return 0;
+    return 1;
 }
 
 int th_cd_signal(struct cd *cd)
@@ -413,14 +516,13 @@ int th_cd_signal(struct cd *cd)
     if (!l_empty(&cd->wait)) {
         th = l_entry(l_first(&cd->wait), struct th, node);
         l_del(&th->node);
-        if (th->state & TH_SLEEP) 
-            l_del(&th->snode);
+        th->state &= ~TH_WAIT_CD;
     }
     pthread_spin_unlock(&cd->spin);
 
     if (th) {
         struct cpu  *cpu = &g_cpu[th->cpu];
-
+        
         append_to_ready(th, cpu);
         pthread_cond_signal(&cpu->cond);
     }
@@ -436,8 +538,7 @@ int th_cd_bcast(struct cd *cd)
         struct cpu  *cpu = &g_cpu[th->cpu];
 
         l_del(&th->node);
-        if (th->state & TH_SLEEP) 
-            l_del(&th->snode);
+        th->state &= ~TH_WAIT_CD;
         pthread_spin_unlock(&cd->spin);
         append_to_ready(th, cpu);
         pthread_cond_signal(&cpu->cond);
@@ -482,6 +583,4 @@ int main()
     }
     return 0;
 }
-
-
 
